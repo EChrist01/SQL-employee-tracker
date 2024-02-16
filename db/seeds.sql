d9489981d0d5d9aa6d123e19f9eb4cb79016d760
EChrist01 <ethanchrist02@gmail.com>
2024-02-15 20:02:05 -0700

    sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,7 +7,7 @@ VALUES
 
 INSERT INTO role (id, title, name, salary, department_id)
 VALUES 
-(1, 'Electrical Engineer', 'John Doe', 114000, 1),
-(2, 'Mechanical Engineer', 'Jane Smith', 111000, 2),
-(3, 'Structural Engineer', 'Michael Johnson', 120000, 3),
-(4, 'Chemical Engineer', 'Emily Brown', 117000, 4);
+(001, 'Electrical Engineer', 'John Doe', 114000, 1),
+(002, 'Mechanical Engineer', 'Jane Smith', 111000, 2),
+(003, 'Structural Engineer', 'Michael Johnson', 120000, 3),
+(004, 'Chemical Engineer', 'Emily Brown', 117000, 4);
